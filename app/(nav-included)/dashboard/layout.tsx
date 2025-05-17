73a2e3ee7fdcc1eb9952a9b6cb85190547f25707
Ankit <kashyap25ankit@gmail.com>
2025-05-18 00:35:16 +0530

    upload video option

--- app/(nav-included)/dashboard/layout.tsx
+++ app/(nav-included)/dashboard/layout.tsx
@@ -8,11 +8,11 @@ export default function DashboardRootLayout({
 }>) {
   return (
     <SidebarProvider>
-      <div className="flex gap-12 h-screen w-screen overflow-hidden">
+      <div className="flex  h-dvh w-screen overflow-hidden">
         <AppSidebar />
-        <main className="flex justify-start gap-12 overflow-auto">
+        <main className="flex flex-col md:flex-row overflow-auto">
           <SidebarTrigger className="top-0 -left-2 text-orange-500" />
-          {children}
+          <div>{children}</div>
         </main>
       </div>
     </SidebarProvider>
--- app/(nav-included)/dashboard/page.tsx
+++ app/(nav-included)/dashboard/page.tsx
@@ -1,5 +1,42 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
+import { CloudUpload } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
 export default function Dashboard() {
-  return <div>Hello</div>;
+  const [val, setVal] = useState<string | undefined>(undefined);
+  const inptRef = useRef<null | HTMLInputElement>(null);
+
+  function handleIconClick() {
+    if (inptRef && inptRef.current) {
+      inptRef.current?.click();
+    }
+  }
+
+  useEffect(() => {
+    console.log(val);
+  }, [val]);
+  return (
+    <div className="relative">
+      <div className="">
+        <Input
+          id="picture"
+          type="file"
+          accept="video/*"
+          ref={inptRef}
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+          className="hidden"
+        />
+
+        <div
+          className="group border shadow-xl text-white cursor-pointer w-fit  p-4 rounded-full bg-orange-500 fixed bottom-12 right-12"
+          onClick={handleIconClick}
+        >
+          <CloudUpload className="group-hover:animate-bounce duration-300" />
+        </div>
+      </div>
+    </div>
+  );
 }
